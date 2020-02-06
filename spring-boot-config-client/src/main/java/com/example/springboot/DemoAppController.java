85d6d32a15439dcf4720a2f4df7e4b2bd8cffef1
aansari3 <anas_ansari@intuit.com>
2020-02-06 12:00:51 +0530

    Map property

--- spring-boot-config-client/src/main/java/com/example/springboot/DemoAppController.java
+++ spring-boot-config-client/src/main/java/com/example/springboot/DemoAppController.java
@@ -3,11 +3,13 @@ package com.example.springboot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.websocket.server.PathParam;
 import java.util.Map;
 
@@ -50,5 +52,15 @@ public class DemoAppController {
         System.out.println("queryMap = "+ query);
         System.out.println("query.get(key) = "+ query.get(key));
         return query.get(key);
+    }
+
+    @Resource
+    private ConfigurableEnvironment environmentProperties;
+
+    @GetMapping("/property")
+    String getProperty(@RequestParam("key") String key ) {
+        System.out.println("key = "+ key);
+        System.out.println("environmentProperties.getProperty(key) = "+ environmentProperties.getProperty(key));
+        return environmentProperties.getProperty(key);
     }
 }
